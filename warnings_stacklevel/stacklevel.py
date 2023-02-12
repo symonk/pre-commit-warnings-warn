381f5611981b0280a82c1c2e1dc45bf8ad3a628d
symonk <jackofspaces@gmail.com>
2023-02-12 13:44:37 +0000

    applying logic in `main` to handle errors and start boilerplate for file reading and ast calling

--- warnings_stacklevel/stacklevel.py
+++ warnings_stacklevel/stacklevel.py
@@ -1,25 +1,56 @@
 import argparse
+import ast
 import dataclasses
 import typing
 
 
-@dataclasses.dataclass
-class FileContainer:
-    """A Container for the command line file paths."""
+class Result(typing.NamedTuple):
+    """Custom sequence for recording violations in files, including their line and column."""
 
-    files: typing.Sequence[str]
+    name: str
+    line: int
+    column: int
+
+
+class WarningsWarnVisitor(ast.NodeVisitor):
+    """A Custom visitor class that can parse python AST syntax looking for
+    references to warning.warn."""
+
+    def __init__(self) -> None:
+        self.violations: typing.List[Result] = []
+
+    def visit_Call(self, node: ast.Call) -> None:
+        print(node)
+
+
+def check_python_file(name: str) -> typing.List[Result]:
+    """Given a file path, open and parse the AST of the file
+    looking for warnings.warn calls that violate the hook.
+
+    :param name: The absolute file path"""
+    with open(name, mode="rb") as f:
+        tree = ast.parse(f.read(), filename=name)
+    custom_visitor = WarningsWarnVisitor()
+    custom_visitor.visit(tree)
+    return custom_visitor.violations
 
 
 def main() -> int:
     """A Pre-commit hook that checks for the default `stacklevel` argument when attempting to
     emit python warnings.  Often stacklevel is left on the default `1` which offers little
     value."""
-    files = parse_argv()
-    print(files)
-    return 0
+    exit_code = 0
+    namespace = parse_argv()
+    for file in namespace.filenames:
+        results = check_python_file(file)
+        if results:
+            exit_code = 1
+        for result in results:
+            print(f"{file}:{result.line}:{result.column}: `warnings.warn(..., stacklevel=1)` detected.")
+    return exit_code
 
 
-def parse_argv() -> FileContainer:
+def parse_argv() -> argparse.Namespace:
     """Convert the command line options into a FileContainer instance."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -29,7 +60,7 @@ def parse_argv() -> FileContainer:
         default=[],
         help="The sequence of tracked staged files",
     )
-    return FileContainer(files=parser.parse_args().filenames)
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
