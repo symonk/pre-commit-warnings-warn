1ac286fd999d4c9128c5942f1237a1100c29e3f0
symonk <jackofspaces@gmail.com>
2023-02-12 12:57:55 +0000

    adding `mypy` and tidying code structure

--- warnings_stacklevel/warning_stacklevel.py
+++ warnings_stacklevel/warning_stacklevel.py
@@ -6,11 +6,12 @@ import typing
 @dataclasses.dataclass
 class FileContainer:
     """A Container for the command line file paths."""
+
     files: typing.Sequence[str]
 
 
 def main() -> int:
-    """A Pre-commit hook that checks for the default `stacklevel` argument when attempting to 
+    """A Pre-commit hook that checks for the default `stacklevel` argument when attempting to
     emit python warnings.  Often stacklevel is left on the default `1` which offers little
     value."""
 
@@ -19,7 +20,10 @@ def main() -> int:
 
 def parse_argv() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--filenames", action="store", required=True, dest="filenames", help="The sequence of tracked staged files")
+    parser.add_argument(
+        "--filenames", action="store", required=True, dest="filenames", help="The sequence of tracked staged files"
+    )
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
