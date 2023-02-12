561f8aa5e31b465be6f38dd8af60cac73df6f13d
symonk <jackofspaces@gmail.com>
2023-02-12 13:30:18 +0000

    update types for `FileContainer`

--- warnings_stacklevel/stacklevel.py
+++ warnings_stacklevel/stacklevel.py
@@ -14,14 +14,13 @@ def main() -> int:
     """A Pre-commit hook that checks for the default `stacklevel` argument when attempting to
     emit python warnings.  Often stacklevel is left on the default `1` which offers little
     value."""
-    files = parse_argv().filenames
+    files = parse_argv()
     print(files)
     return 0
 
 
-def parse_argv() -> argparse.Namespace:
-    """Handle parsing the command line arguments.  pre-commit implicitly is handling
-    the filenames sequence, we just parse it internally when provided here."""
+def parse_argv() -> FileContainer:
+    """Convert the command line options into a FileContainer instance."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "filenames",
