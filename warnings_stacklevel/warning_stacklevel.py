afb266c9d7214d58ddc0d1670bc7e45d444a27c3
symonk <jackofspaces@gmail.com>
2023-02-12 13:04:19 +0000

    adding required `language` to hook and running it against the code locally

--- warnings_stacklevel/warning_stacklevel.py
+++ warnings_stacklevel/warning_stacklevel.py
@@ -14,14 +14,15 @@ def main() -> int:
     """A Pre-commit hook that checks for the default `stacklevel` argument when attempting to
     emit python warnings.  Often stacklevel is left on the default `1` which offers little
     value."""
-
+    breakpoint()
+    files = parse_argv().filenames
     return 0
 
 
 def parse_argv() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--filenames", action="store", required=True, dest="filenames", help="The sequence of tracked staged files"
+        "--filenames", action="store", nargs="+", default=[], dest="filenames", help="The sequence of tracked staged files"
     )
     return parser.parse_args()
 
